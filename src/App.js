a1e5162abb580de82057d188a6aad7e2398743d6
amandt23 <149380219+amandt23@users.noreply.github.com>
2023-11-25 06:59:18 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 import Price from './components/Price/Price';
 import Review from './components/Reviews/Review';
 import Social from './components/Social/Social';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <MyTeam />
       <PlaceOrder />
       <Price />
